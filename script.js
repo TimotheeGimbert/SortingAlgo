aaca5e388ef5fa47a7aa192502b0edaa02b1b71a
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-12 00:32:51 +0100

    first algo

---
+++ script.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+// console.log(process.argv);
+const fileName = process.argv[2];
+
+let data = [];
+try {
+  data = fs.readFileSync(fileName, 'utf8');
+  console.log(data);
+} catch (error) {
+  console.error(error.message);
+}
+
+data = data.split(' ');
+const dataInt = data.map(element => parseInt(element));
+console.log(data);
+
+const bubbleSort = (numbers) => {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let k = 1; k < numbers.length; k++) {
+      if (numbers[k] < numbers[k-1]) {
+        let kTemp = numbers[k];
+        numbers[k] = numbers[k-1];
+        numbers[k-1] = kTemp;
+      }
+    }
+  }
+  return numbers;
+}
+
+console.log(bubbleSort(dataInt));
